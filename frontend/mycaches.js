33ea2fb6510fa7185a3049c9b19fec2ac1f217cc
Borek Lupomesky <borek@lupomesky.cz>
2020-04-20 21:04:56 +0200

    Make frontend load data from backend

--- frontend/mycaches.js
+++ frontend/mycaches.js
@@ -1,9 +1,24 @@
+function getList()
+{
+  return fetch('mycaches.cgi')
+  .then(response => {
+    if(!response.ok) {
+      throw new Error(`Failed to retrieve data (${response.statusText})`);
+    }
+    return response.json();
+  });
+}
+
+
 document.addEventListener("DOMContentLoaded", function(event) {
 
-  var vm = new Vue({
-    el: '#app',
-    data: {
-    },
+  getList().then(data => {
+    var vm = new Vue({
+      el: '#app',
+      data: {
+        d: data,
+      },
+    });
   });
 
 });
