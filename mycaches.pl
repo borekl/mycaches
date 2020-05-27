d0f5ea4624256746e3cf65f964fe0c944c3022b5
Borek Lupomesky <borek@lupomesky.cz>
2020-05-27 09:35:49 +0200

    Rewrite the whole thing to use server-side templating
    The client-side SPA is overkill for something that's a simple list pulled
    out of a database.  It also has some problems: 1) It loads after the page
    has already been rendered for the first time, causing unsightly flash of
    content. 2) One cannot link inside the page with "#id", this would need to
    be solved with routes in the SPA, adding even more unnecessary complexity.
    
    Therefore the whole thing now uses server-side templating (using Template
    Toolkit 2) and client receives finished page.

---
+++ mycaches.pl
@@ -0,0 +1,109 @@
+#!/usr/bin/env plackup
+
+use strict;
+use warnings;
+use utf8;
+use feature 'state';
+
+use FindBin qw($Bin);
+use JSON::MaybeXS;
+use DBI;
+use Plack::Request;
+use Plack::Response;
+use Template;
+use Encode qw(encode);
+use Time::Moment;
+
+
+my $app = sub {
+  my $req = Plack::Request->new(shift);
+  my $res = Plack::Response->new(200);
+  my $json = decode_json($req->content) if $req->content;
+  my $now = Time::Moment->now_utc;
+  state $tt  = Template->new({ INCLUDE_PATH => "$Bin", ENCODING => 'utf8'  });
+
+  my $dbfile = 'mycaches.sqlite';
+  my $dbh = DBI->connect("dbi:SQLite:dbname=$dbfile","","");
+  $dbh->{sqlite_unicode} = 1;
+
+  my $finds = $dbh->selectall_arrayref(
+    'SELECT * FROM finds ORDER BY finds_i',
+    { Slice => {} }
+  );
+
+  my $hides = $dbh->selectall_arrayref(
+    'SELECT * FROM hides ORDER BY hides_i',
+    { Slice => {} }
+  );
+
+  #--- calculate age/held fields for finds
+
+  foreach (@$finds) {
+
+    # ignore lab caches
+    next if $_->{ctype} eq 'L';
+
+    # calculate 'age', ie. numer of days since previous find
+    my $previous_find = Time::Moment->from_string($_->{prev} . 'T00Z');
+    my $found = Time::Moment->from_string($_->{found} . 'T00Z');
+    $_->{age} = $previous_find->delta_days($found);
+
+    # calculate 'held', ie. number of days when I was the last finder
+    my $next_find = $now->at_midnight;
+    $next_find = Time::Moment->from_string($_->{next} . 'T00Z') if $_->{next};
+    $_->{held} = $found->delta_days($next_find);
+  }
+
+  #--- calculate age for hides
+
+  foreach (@$hides) {
+
+    # ignore unpublished hides
+    next if !$_->{published};
+
+    my $last_found = Time::Moment->from_string($_->{found} . 'T00Z')
+      if $_->{found};
+    my $published = Time::Moment->from_string($_->{published} . 'T00Z')
+      if $_->{published};
+    my $timeref = $last_found // $published;
+
+    $_->{age} = $timeref->at_midnight->delta_days($now->at_midnight);
+  }
+
+  #--- run the data through a template
+
+  my $out;
+  $tt->process(
+    'mycaches.tt',
+    { finds => $finds, hides => $hides },
+    \$out
+  ) or $out = $tt->error;
+
+  #--- get expiration time
+
+  # we're setting expiration 10 minutes to the future, with the exception
+  # that midnight always expires the page (since the age/held fields increase
+  # exactly at midnight)
+
+  my $expire = $now->plus_minutes(10);
+  my $next_midnight = $now->plus_days(1)->at_midnight;
+  $expire = $expire < $next_midnight ? $expire : $next_midnight;
+
+  #--- set refresh timeout
+
+  # we're setting the page to refresh on every midnight; this will make the
+  # page stay up-to-date without user intervention
+
+  my $seconds_to_midnight = $now->delta_seconds($next_midnight);
+  $seconds_to_midnight ||= 86400;
+
+  #--- finish
+
+  $res->headers([
+    'Content-Type' => 'text/html; charset=utf-8',
+    'Expires' => $expire->strftime('%a, %d %b %Y %H:%M:%S GMT'),
+    'Refresh' => $seconds_to_midnight,
+  ]);
+  $res->body(encode('UTF-8', $out));
+  $res->finalize;
+};
